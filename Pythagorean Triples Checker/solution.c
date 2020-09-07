80456308c573674cccf7c20cb33013bc973c4f70
Ratheshprabakar <ratheshprabakar@gmail.com>
2020-09-07 19:18:31 +0530

    Added Loop the program condition in solution.c

--- Pythagorean Triples Checker/solution.c
+++ Pythagorean Triples Checker/solution.c
@@ -3,11 +3,13 @@ int a,b,c,max,result;
 int pythogorean_triples_checker(int max,int a, int b);
 int main()
 {
-	
-	printf("Enter the three sides\t");
-	scanf("%d\t%d\t%d",&a,&b,&c);
-	result=(a>b)?((a>c)?pythagorean_triples_checker(b,c,a):pythagorean_triples_checker(a,b,c)):((b>c)?pythagorean_triples_checker(a,c,b):pythagorean_triples_checker(a,b,c));
-	(result==1)?printf("Pythagorean Triple"):printf("Not Pythagorean Triple");
+	while(1)
+	{
+		printf("\n\nEnter the three sides\t");
+		scanf("%d\t%d\t%d",&a,&b,&c);
+		result=(a>b)?((a>c)?pythagorean_triples_checker(b,c,a):pythagorean_triples_checker(a,b,c)):((b>c)?pythagorean_triples_checker(a,c,b):pythagorean_triples_checker(a,b,c));
+		(result==1)?printf("Pythagorean Triple"):printf("Not Pythagorean Triple");
+	}
 }
 int pythagorean_triples_checker(a,b,max)
 {
